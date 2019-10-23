919f9fb90b9068a092a54d3d4a2ecd6e1388eb91
Breno Rodrigues <brenorodrigues.smb@gmail.com>
2019-10-22 23:29:08 -0300

    first commit

---
+++ resources/views/site/master/app.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,300">
+    <link rel="stylesheet" href="/css/master.css">
+</head>
+<body>
+
+@yield('master')
+
+<script src="/js/jquery.1.11.2.js"></script>
+<script src="/js/bootstrap.js"></script>
+<script src="/js/function.js"></script>
+<script src="/js/bootstrap-datepicker.js"></script>
+<script src="/js/parallax.js"></script>
+<script src="/js/scorll.js"></script>
+<script src="/js/jquery.easing.min.js"></script>
+<script src="/js/slick.js"></script>
+<script src="/js/menu.js"></script>
+<script src="/js/ios-timer.js"></script>
+<script src="/js/jquery.fencybox.js"></script>
+<script src="/js/jquery.portfolio.js"></script>
+<script src="/js/jquery.mousewheel-3.0.6.pack.js"></script>
+<script src="/js/wow.js"></script>
+<script src="/js/jquery.validate.js"></script>
+<script src="/js/revoluation/jquery.themepunch.tools.min.js"></script>
+<script src="/js/revoluation/jquery.themepunch.revolution.min.js"></script>
+<script src="/js/revoluation/revolution.extension.layeranimation.min.js"></script>
+<script src="/js/revoluation/revolution.extension.migration.min.js"></script>
+<script src="/js/revoluation/revolution.extension.navigation.min.js"></script>
+<script src="/js/revoluation/revolution.extension.parallax.min.js"></script>
+<script src="/js/revoluation/revolution.extension.slideanims.min.js"></script>
+<script src="/js/revoluation/revoluationfunction.js"></script>
+</body>
+</html>
